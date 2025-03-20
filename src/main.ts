e060a3dfa35ca8ab5719423a552e835d35e55e87
Victor Saisse <saissevictor@gmail.com>
2025-03-19 20:36:25 -0400

    enableCors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from 'src/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(parseInt(process.env.PORT!));
   console.log(`[OK] Server is running on port ${process.env.PORT}`);
 }
