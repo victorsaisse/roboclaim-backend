744925d1b065cbf9801a9811447e089c1fcc9aa9
Victor Saisse <saissevictor@gmail.com>
2025-03-20 14:05:54 -0400

    concurrency test

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Delete, Get, Param, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { Public } from 'src/decorators/public.decorator';
 import { Role } from 'src/decorators/roles.decorator';
 import { User } from 'src/entities/user.entity';
 import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
@@ -20,5 +28,25 @@ export class UserController {
   @Role('admin')
   delete(@Param('id') id: string): Promise<void> {
     return this.userService.delete(id);
+  }
+
+  @Public()
+  @Get('wait')
+  async wait(
+    @Query('seconds') seconds: number,
+    @Query('name') name: string,
+  ): Promise<string> {
+    let elapsedSeconds = 0;
+    await new Promise((resolve) => {
+      const interval = setInterval(() => {
+        elapsedSeconds++;
+        console.log(`Waited ${elapsedSeconds} seconds for ${name}`);
+        if (elapsedSeconds >= seconds) {
+          clearInterval(interval);
+          resolve(null);
+        }
+      }, 1000);
+    });
+    return `Waited ${seconds} seconds`;
   }
 }
